53de08dd39da4782681a7cf339bd3bd327023d85
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-12 15:09:32 +0530

    Composit Pattern Problem

---
+++ com/critional/deaign/composit/Directory.java
@@ -0,0 +1,37 @@
+package com.critional.deaign.composit;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory {
+    String directoryName;
+    List<Object> objectList;
+    public Directory(String name){
+        this.directoryName = name;
+        objectList = new ArrayList<>();
+
+    }
+    public void add(Object object){
+        objectList.add(object);
+
+    }
+    public void remove(Object object){
+        objectList.remove(object);
+    }
+
+    // Display full Structure
+    //Breaks  OCP - if we want to add a new file type, we need to modify this method to add another if/else condition
+
+    public void printContents(){
+        System.out.println("Directory Name: "+ directoryName);
+        for(Object obj : objectList){
+            if(obj instanceof File){
+                ((File) obj).printContents();
+
+            }else if(obj instanceof  Directory){
+                ((Directory) obj).printContents();
+            }
+        }
+    }
+}
