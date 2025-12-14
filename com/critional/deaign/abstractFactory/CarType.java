8b493cbd4d188170f3f4a02a97b818566f5f97ed
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-14 19:23:26 +0530

    AbstractMethods

---
+++ com/critional/deaign/abstractFactory/CarType.java
@@ -0,0 +1,20 @@
+package com.critional.deaign.abstractFactory;
+
+public enum CarType {
+    ECONOMY, LUXURY, PREMIUM
+}
+// Step 6: Factory Provider
+public class CarFactoryProvider {
+    public CarFactory getFactory(CarType type, String brand) {
+        switch (type) {
+            case ECONOMY:
+                return new EconomyCarFactory(brand);
+            case PREMIUM:
+            case LUXURY:
+                return new LuxuryCarFactory(brand);
+            default:
+                throw new IllegalArgumentException("Unknown car type: " + type);
+        }
+    }
+}
+
