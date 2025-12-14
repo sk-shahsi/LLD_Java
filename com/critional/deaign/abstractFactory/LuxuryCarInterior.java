095d4889cbace5ed6323e5b8c0cbb05e9b567706
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-14 19:33:06 +0530

    AbstractMethods

---
+++ com/critional/deaign/abstractFactory/LuxuryCarInterior.java
@@ -0,0 +1,10 @@
+package com.critional.deaign.abstractFactory;
+// Step 3: Concrete Products for Luxury Car Family
+public class LuxuryCarInterior implements CarInterior {
+    @Override
+    public void addInteriorComponents() {
+        System.out.println("Adding luxurious interior components for Luxury Car.");
+    }
+}
+
+
