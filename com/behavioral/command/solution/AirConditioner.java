7a7c2c5c95b34f9e24ed87a17e6a40a0531c4ed8
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-22 22:11:02 +0530

    command pattern solution

---
+++ com/behavioral/command/solution/AirConditioner.java
@@ -0,0 +1,34 @@
+package com.behavioral.command.solution;
+
+public class AirConditioner {
+    boolean isOn;
+    int temperature;
+
+    public void turnOn() {
+        isOn = true;
+        System.out.println("Air conditioner is on");
+    }
+
+    public void turnOff() {
+        isOn = false;
+        System.out.println("Air conditioner is off");
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+        System.out.println("Air conditioner temperature set to " + temperature + "°C");
+    }
+
+}
