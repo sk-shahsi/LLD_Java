9da2f54d9ca7f4fe456966442728984322c7ff82
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-30 00:26:25 +0530

    state desigen pattern

---
+++ com/behavioral/state/vendingmachinestates/impl/IdleState.java
@@ -0,0 +1,26 @@
+package com.behavioral.state.vendingmachinestates.impl;
+
+import com.behavioral.state.context.VendingMachine;
+
+public class IdleState extends State {
+
+    public IdleState(){
+        System.out.println("Currently Vending machine is in IdleState");
+    }
+
+    public IdleState(VendingMachine machine){
+        System.out.println("Currently Vending machine is in IdleState");
+        machine.setCoinList(new ArrayList<>());
+    }
+
+    @Override
+    public void clickOnInsertCoinButton(VendingMachine machine) throws Exception{
+        machine.setVendingMachineState(new HasMoneyState());
+    }
+
+    @Override
+    public void updateInventory(VendingMachine machine, Item item, int codeNumber) throws Exception {
+        machine.getInventory().addItem(item, codeNumber);
+    }
+
+}
